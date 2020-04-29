3caf86a14f899f38abde2456034056b42deba707
Omar Diaz <tecnologia@somosmascuba.com>
2020-04-29 17:32:30 +0200

    Deploy

--- manager/settings/live.py
+++ manager/settings/live.py
@@ -11,7 +11,7 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_S3_REGION_NAME = get_secret('AWS_REGION')
 AWS_S3_BUCKET_AUTH = False
 AWS_S3_MAX_AGE_SECONDS = int(timedelta(days=7).total_seconds())
-AWS_STORAGE_BUCKET_NAME = get_secret('AWS_S3_BUCKET_NAME')
+AWS_STORAGE_BUCKET_NAME = 'somosmas-management'
 AWS_ACCESS_KEY_ID = get_secret('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = get_secret('AWS_SECRET_ACCESS_KEY')
 AWS_S3_SIGNATURE_VERSION = 's3'
