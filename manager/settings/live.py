c63364b069dccb577c290ee3a34171c4244938a9
Omar Diaz <tecnologia@somosmascuba.com>
2020-06-14 12:41:42 +0200

    Refactor mass mail

--- manager/settings/live.py
+++ manager/settings/live.py
@@ -13,7 +13,7 @@ AWS_S3_BUCKET_AUTH = False
 AWS_S3_MAX_AGE_SECONDS = int(timedelta(days=7).total_seconds())
 AWS_STORAGE_BUCKET_NAME = 'somosmas-management'
 AWS_ACCESS_KEY_ID = get_secret('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = get_secret('AWS_SECRET_ACCESS_KEY')
+AWS_SECRET_ACCESS_KEY = get_secret('ERP_AWS_SECRET_ACCESS_KEY')
 AWS_S3_SIGNATURE_VERSION = 's3v4'
 
 DATABASES = {
@@ -21,7 +21,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'HOST': get_secret('DB_HOST'),
         'USER': get_secret('DB_USER'),
-        'PASSWORD': get_secret('DB_PASSWORD'),
+        'PASSWORD': get_secret('ERP_DB_PASSWORD'),
         'NAME': get_secret('DB_NAME'),
         'ATOMIC_REQUESTS': True,
     }
@@ -29,13 +29,13 @@ DATABASES = {
 
 DEBUG = False
 
-EMAIL_HOST_PASSWORD = get_secret('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_PASSWORD = get_secret('ERP_EMAIL_HOST_PASSWORD')
 EMAIL_HOST = get_secret('EMAIL_HOST')
 EMAIL_HOST_USER = get_secret('EMAIL_HOST_USER')
 EMAIL_PORT = get_secret('EMAIL_PORT')
 REGISTRATION_DEFAULT_FROM_EMAIL = get_secret('REGISTRATION_DEFAULT_FROM_EMAIL')
 
-PAYPAL_CLIENT_SECRET = get_secret('PAYPAL_CLIENT_SECRET')
+PAYPAL_CLIENT_SECRET = get_secret('ERP_PAYPAL_CLIENT_SECRET')
 PAYPAL_MODE = get_secret('PAYPAL_MODE')
 PAYPAL_CLIENT_ID = get_secret('PAYPAL_CLIENT_ID')
 PAYPAL_CREATE_PRODUCT_URL = get_secret('PAYPAL_CREATE_PRODUCT_URL')
@@ -50,12 +50,12 @@ sentry_sdk.init(
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 DEFAULT_FROM_EMAIL = 'tecnologia@somosmascuba.com'
-SENDGRID_API_KEY = get_secret('SENDGRID_API_KEY')
+SENDGRID_API_KEY = get_secret('ERP_SENDGRID_API_KEY')
 
 EMAIL_BACKEND = 'sendgrid_backend.SendgridBackend'
 
-RECAPTCHA_PUBLIC_KEY = get_secret('RECAPTCHA_PUBLIC_KEY', 'fake')
-RECAPTCHA_PRIVATE_KEY = get_secret('RECAPTCHA_PRIVATE_KEY', 'fake')
+RECAPTCHA_PUBLIC_KEY = get_secret('ERP_RECAPTCHA_PUBLIC_KEY', 'fake')
+RECAPTCHA_PRIVATE_KEY = get_secret('ERP_RECAPTCHA_PRIVATE_KEY', 'fake')
 SILENCED_SYSTEM_CHECKS = []
 
 
