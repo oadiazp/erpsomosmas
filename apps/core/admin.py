c1d293d9a73566c80eecb57fde6c5c6e40d6950e
Omar Diaz <tecnologia@somosmascuba.com>
2020-09-15 21:15:42 +0200

    Fixing the admin action for set clubs

--- apps/core/admin.py
+++ apps/core/admin.py
@@ -90,7 +90,7 @@ class ClubAdmin(admin.ModelAdmin):
         'find_best_matches_to_all_members',
     )
 
-    def find_best_matches_to_all_members(self):
+    def find_best_matches_to_all_members(self, request, queryset):
         member: Profile
         for member in Profile.objects.members():
             BestClubMatcher.find(member)
