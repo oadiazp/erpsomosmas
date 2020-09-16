f132c2c9eb157b94334bfc8862d8ec8eb013818f
Omar Diaz <tecnologia@somosmascuba.com>
2020-09-16 22:45:40 +0200

    Club filter

--- apps/core/management/commands/set_members_club.py
+++ apps/core/management/commands/set_members_club.py
@@ -6,7 +6,7 @@ from apps.core.models import Profile, BestClubMatcher
 class Command(BaseCommand):
     def handle(self, *args, **options):
         member: Profile
-        for member in Profile.objects.members():
+        for member in Profile.objects.members().filter(club=None):
             club = BestClubMatcher.find(member)
             member.club = club
             member.save()
